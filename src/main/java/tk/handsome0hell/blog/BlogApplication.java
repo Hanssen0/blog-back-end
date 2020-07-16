6e98c505033d629777320a78a69a038960c72ce5
handsome0hell <handsome0hell@gmail.com>
2020-07-16 22:22:27 +0800

    Allow cors for all domain

--- src/main/java/tk/handsome0hell/blog/BlogApplication.java
+++ src/main/java/tk/handsome0hell/blog/BlogApplication.java
@@ -3,11 +3,22 @@ package tk.handsome0hell.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 public class BlogApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
-
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
+      }
+    };
+  }
 }
