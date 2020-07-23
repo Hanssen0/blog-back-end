1cdd292a8579155901a9024124b5c8acc7b88cec
handsome0hell <handsome0hell@gmail.com>
2020-07-23 23:16:14 +0800

    Add getting all users method to UsersRepository

--- src/main/java/tk/handsome0hell/blog/security/UsersRepository.java
+++ src/main/java/tk/handsome0hell/blog/security/UsersRepository.java
@@ -1,7 +1,9 @@
 package tk.handsome0hell.blog.security;
 
 import tk.handsome0hell.blog.pojo.User;
+import java.util.List;
 
 interface UsersRepository {
   Boolean ValidateUser(User user);
+  List<User> GetUsers();
 };
