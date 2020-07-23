a8d1bd5cd5361155b32108ac3ecdf4a6b07bae0a
handsome0hell <handsome0hell@gmail.com>
2020-07-23 23:17:23 +0800

    Add getting all users API

--- src/main/java/tk/handsome0hell/blog/web/UsersPresenter.java
+++ src/main/java/tk/handsome0hell/blog/web/UsersPresenter.java
@@ -8,6 +8,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import tk.handsome0hell.blog.pojo.User;
 import tk.handsome0hell.blog.security.UsersComponent;
+import java.util.List;
+import java.util.LinkedList;
+
+class UserWithoutPassword {
+  private String username;
+  public UserWithoutPassword(User user) {
+    this.username = user.getUsername();
+  }
+  public String getUsername() {return username;}
+};
 
 @RestController
 @RequestMapping("/users")
@@ -27,5 +37,15 @@ public class UsersPresenter {
   @DeleteMapping("login")
   public Boolean Logout() {
     return users_component.Logout();
+  };
+  @GetMapping("")
+  public List<UserWithoutPassword> GetUsers() {
+    List<UserWithoutPassword> response_users =
+      new LinkedList<UserWithoutPassword>();
+    List<User> users = users_component.GetUsers();
+    for (User user : users) {
+      response_users.add(new UserWithoutPassword(user));
+    }
+    return response_users;
   };
 }
