05225cf9385e685d5a213e8b4aa1b358d5b94941
handsome0hell <handsome0hell@gmail.com>
2020-07-23 23:20:24 +0800

    Add id property to User class

--- src/main/java/tk/handsome0hell/blog/pojo/User.java
+++ src/main/java/tk/handsome0hell/blog/pojo/User.java
@@ -1,12 +1,15 @@
 package tk.handsome0hell.blog.pojo;
 
 public class User {
+  private Integer id;
   private String username;
   private String password;
   public User(String username, String password) {
     this.username = username;
     this.password = password;
   }
+  public void setId(Integer id) {this.id = id;}
+  public Integer getId() {return id;}
   public String getUsername() {return this.username;}
   public String getPassword() {return this.password;}
 }
