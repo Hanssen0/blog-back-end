24e0fc67bb7954aadfdba5ae95930f6cb939181d
handsome0hell <handsome0hell@gmail.com>
2020-07-25 01:18:54 +0800

    Add pojo Role class

---
+++ src/main/java/tk/handsome0hell/blog/pojo/Role.java
@@ -0,0 +1,11 @@
+package tk.handsome0hell.blog.pojo;
+
+public class Role {
+  private Integer id;
+  private String name;
+  public Role() {}
+  public void setId(Integer id) {this.id = id;}
+  public void setName(String name) {this.name = name;}
+  public Integer getId() {return id;}
+  public String getName() {return name;}
+}
