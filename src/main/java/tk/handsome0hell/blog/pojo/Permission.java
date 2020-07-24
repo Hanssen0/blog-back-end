18945b78f963a09af2a1c1b7e472c6b0a9549274
handsome0hell <handsome0hell@gmail.com>
2020-07-25 01:19:10 +0800

    Add pojo Permission class

---
+++ src/main/java/tk/handsome0hell/blog/pojo/Permission.java
@@ -0,0 +1,8 @@
+package tk.handsome0hell.blog.pojo;
+
+public class Permission {
+  private Integer id;
+  public Permission() {}
+  public void setId(Integer id) {this.id = id;}
+  public Integer getId() {return id;}
+}
