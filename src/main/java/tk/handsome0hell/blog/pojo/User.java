028a82f383db446f3c20bd43e3d164d6dbb98706
handsome0hell <handsome0hell@gmail.com>
2020-07-24 22:00:16 +0800

    Add role_id property to User

--- src/main/java/tk/handsome0hell/blog/pojo/User.java
+++ src/main/java/tk/handsome0hell/blog/pojo/User.java
@@ -4,11 +4,15 @@ public class User {
   private Integer id;
   private String username;
   private String password;
+  private Integer role_id;
   public User() {}
   public void setId(Integer id) {this.id = id;}
   public void setUsername(String username) {this.username = username;}
   public void setPassword(String password) {this.password = password;}
+  public void setRole_id(Integer role_id) {this.role_id = role_id;}
   public Integer getId() {return id;}
   public String getUsername() {return username;}
   public String getPassword() {return password;}
+  public Integer getRole_id() {return role_id;}
+
 }
