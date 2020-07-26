a84497d758559f0d5e06607ef165d6b0f3121067
handsome0hell <handsome0hell@gmail.com>
2020-07-26 17:20:18 +0800

    Return ResponseBody in ArticlesPresenter class

--- src/main/java/tk/handsome0hell/blog/web/ArticlesPresenter.java
+++ src/main/java/tk/handsome0hell/blog/web/ArticlesPresenter.java
@@ -1,12 +1,12 @@
 package tk.handsome0hell.blog.web;
 
-import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import tk.handsome0hell.blog.pojo.ResponseBody;
 import tk.handsome0hell.blog.pojo.Article;
 import tk.handsome0hell.blog.articles.ArticlesComponent;
 
@@ -18,16 +18,20 @@ public class ArticlesPresenter {
     this.articles_component = articles_component;
   }
   @GetMapping("")
-  public List<Article> GetArticles() {
-    return articles_component.GetArticles();
+  public ResponseBody GetArticles() {
+    return new ResponseBody(articles_component.GetArticles());
   }
   @GetMapping("{id}")
-  public Article GetArticle(@PathVariable("id") Integer id) {
-    return articles_component.GetArticleById(id);
+  public ResponseBody GetArticle(@PathVariable("id") Integer id) {
+    // TODO: Handle article not fund error
+    return new ResponseBody(articles_component.GetArticleById(id));
   }
   @PutMapping("{id}")
-  public void PutArticle(@PathVariable("id") Integer id, @RequestBody Article article) {
+  public ResponseBody PutArticle(
+      @PathVariable("id") Integer id,
+      @RequestBody Article article) {
     article.setId(id);
     articles_component.UpdateArticle(article);
+    return new ResponseBody();
   }
 }
